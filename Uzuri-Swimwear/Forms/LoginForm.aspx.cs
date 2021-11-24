2ce7e97c670bfbbf899cf4c2c3dd9920800bfbbe
Aidan Joffee <19011337@vcconnect.co.za>
2021-11-24 16:08:16 +0200

    Login form

--- Uzuri-Swimwear/Forms/LoginForm.aspx.cs
+++ Uzuri-Swimwear/Forms/LoginForm.aspx.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Uzuri_Swimwear.Model;
 
 namespace Uzuri_Swimwear.Forms
 {
@@ -11,7 +13,61 @@ namespace Uzuri_Swimwear.Forms
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                if (User.Identity.IsAuthenticated)
+                {
+                    //display name
+                }
+                else
+                {
+                    //hide login form
+                }
+            }
+        }
+        protected void SignIn(object sender, EventArgs e)
+        {
+            SignInUser();
+        }
 
+        protected void SignInUser()
+        {
+            if (IsValid)
+            {
+                // Validate the user password
+                var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var signinManager = Context.GetOwinContext().GetUserManager<ApplicationSignInManager>();
+
+                // This doen't count login failures towards account lockout
+                // To enable password failures to trigger lockout, change to shouldLockout: true
+                var result = signinManager.PasswordSignIn(Email.Text, Password.Text, RememberMe.Checked, shouldLockout: true);
+
+                switch (result)
+                {
+                    case SignInStatus.Success:
+                        Response.Redirect("~/Forms/HomeForm.aspx");
+                        break;
+                    case SignInStatus.LockedOut:
+                        //TODO create these pages redirect to lockout
+
+                        break;
+                    case SignInStatus.RequiresVerification:
+                        //TODO create these pages to require verification if necessary?
+                        break;
+                    case SignInStatus.Failure:
+                    default:
+                        FailureText.Text = "Invalid login attempt";
+                        ErrorMessage.Visible = true;
+                        break;
+                }
+            }
+        }
+
+        protected void SignOut(object sender, EventArgs e)
+        {
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+            authenticationManager.SignOut();
+            Response.Redirect("~/Forms/Login/LoginPage.aspx");
         }
     }
 }
