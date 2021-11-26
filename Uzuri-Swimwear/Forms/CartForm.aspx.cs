53bddcf58a03e2f8eb3f3f869c23864e98c3748e
Adam Schoeman <19002380@vcconnect.co.za>
2021-11-26 12:42:35 +0200

    removed old code

--- Uzuri-Swimwear/Forms/CartForm.aspx.cs
+++ Uzuri-Swimwear/Forms/CartForm.aspx.cs
@@ -71,32 +71,7 @@ namespace Uzuri_Swimwear.Forms
             }
         }
 
-        //protected void GetProdId(object sender, ListViewCommandEventArgs e)
-        //{
-        //    ListViewDataItem lvd = (ListViewDataItem)((Control)e.CommandSource).NamingContainer;
-
-        //    //Same two lines for each value
-        //    Label ID = lvd.FindControl("CartProdId") as Label;
-        //    string id = ID.ToString();
-        //    Response.Write(id);
-        //}
-
-        /*
-        protected void UpdateButton_Click(object sender, EventArgs e)
-        {
-            //cast the sender back to a button
-            Button btn = sender as Button;
-
-            //get the current item from the listview namingcontainer
-            ListViewItem item = btn.NamingContainer as ListViewItem;
-
-            //use findcontrol to locate the label in that item
-            Label lbl = item.FindControl("CartProdId") as Label;
-
-            //show result in label outside listview
-            Response.Write(lbl.Text);
-        }
-        */
+        
         protected void listViewCartProducts_ItemCommand(object sender, ListViewCommandEventArgs e)
         {
             if(e.CommandName == "Remove")
